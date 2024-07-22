996b660d9fd3031056c2a3fccca61fa7f7e97e38
Evelyn Zapata <103339379+EvelynZapata20@users.noreply.github.com>
2024-07-21 21:07:32 -0500

    Add files via upload

---
+++ primera_app_php/form.php
@@ -0,0 +1,31 @@
+<?php
+include 'db.php';
+
+$data = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['nombre'])) {
+        $nombre = $conn->real_escape_string($_POST['nombre']);
+
+        $sql = "INSERT INTO usuario (nombre) VALUES ('$nombre')";
+        $result = $conn->query($sql);
+
+        if ($result) {
+            $sql = "SELECT nombre FROM usuario";
+            $result = $conn->query($sql);
+
+            while($row = $result->fetch_assoc()) {
+                $data .= "<tr><td>" . htmlspecialchars($row["nombre"]) . "</td></tr>";
+            }
+        }
+    } else {
+        echo "No se ha proporcionado el nombre<br>";
+    }
+}
+else {
+    echo "No se ha enviado un POST<br>";
+}
+
+include('list.html');
+?>
+
